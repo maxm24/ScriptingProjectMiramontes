c78ee31a824e03f5a445e340866fd0ab171cd760
maxm24 <709003@student.dusd.net>
2023-01-11 15:28:29 -0800

    finished another script
    enabling and disabling script

---
+++ ScriptingProjectMiramontes/Assets/Scripts/EnablingAndDisabling.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablingAndDisabling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
